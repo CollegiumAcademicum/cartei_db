ff1f530744a1a8d50cf2f6dfbd3afb9f815873d3
Philipp <162928651+philipp-btz@users.noreply.github.com.>
2026-08-05 00:41:56 +0200

    feat: models package init and Alembic setup

---
+++ cartei_db/models/__init__.py
@@ -0,0 +1,13 @@
+from cartei_db.models.building import Building
+from cartei_db.models.wg import WG
+from cartei_db.models.room import Room
+from cartei_db.models.tenant import Tenant
+from cartei_db.models.tenant_room_assignment import TenantRoomAssignment
+from cartei_db.models.ag_abfrage import AGAbfrage
+from cartei_db.models.ag_abfrage_result import AGAbfrageResult
+from cartei_db.models.enrollment_proof import EnrollmentProof
+
+__all__ = [
+    "Building", "WG", "Room", "Tenant", "TenantRoomAssignment",
+    "AGAbfrage", "AGAbfrageResult", "EnrollmentProof",
+]
